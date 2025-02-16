6a2d8fa21bea9f57b51868a80d4710973e9b28b4
yapkm01 <yapkm01@hotmail.com>
2025-02-16 12:07:04 -0500

    initial

---
+++ chapter1/hello_cargo/target/debug/deps/hello_cargo-5c2e49fc2710bce3.d
@@ -0,0 +1,5 @@
+/home/yapkm01/Software.Development/Rust/chapter1/hello_cargo/target/debug/deps/hello_cargo-5c2e49fc2710bce3: src/main.rs
+
+/home/yapkm01/Software.Development/Rust/chapter1/hello_cargo/target/debug/deps/hello_cargo-5c2e49fc2710bce3.d: src/main.rs
+
+src/main.rs:
---
+++ chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/yapkm01/Software.Development/Rust/chapter1/hello_cargo/target/debug/hello_cargo: /home/yapkm01/Software.Development/Rust/chapter1/hello_cargo/src/main.rs
